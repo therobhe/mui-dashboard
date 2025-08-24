f854121e613d8442c18c6d416c3fcac66f8005a1
robhe <rheinemann97@gmail.com>
2025-08-24 16:03:50 +0200

    Introduce framer motion animation on component transition in RouteWrapper.tsx

--- src/components/routes/RouteWrapper.tsx
+++ src/components/routes/RouteWrapper.tsx
@@ -1,12 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { HeaderNavigation } from "../Navigation/HeaderNavigation";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function RouteWrapper() {
+	const location = useLocation();
+
 	return (
 		<>
-			<HeaderNavigation/>
+			<HeaderNavigation />
 			<main>
-				<Outlet/>
+				<AnimatePresence mode="wait">
+					<motion.div
+						key={location.pathname}
+						initial={{ opacity: 0, x: 100 }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0, x: -100 }}
+						transition={{ duration: 0.4 }}
+					>
+						<Outlet />
+					</motion.div>
+				</AnimatePresence>
 			</main>
 		</>
 	)
